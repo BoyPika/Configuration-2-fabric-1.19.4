23d80104f9ed14c335ae20eceed28465bd32b4d3
BoyPika <boypika85@gmail.com>
2023-08-18 17:44:41 -0400

    Commit

--- src/main/java/dev/toma/configuration/Configuration.java
+++ src/main/java/dev/toma/configuration/Configuration.java
@@ -1,26 +1,16 @@
 package dev.toma.configuration;
 
-import dev.toma.configuration.client.screen.ConfigGroupScreen;
-import dev.toma.configuration.client.screen.ConfigScreen;
 import dev.toma.configuration.config.Config;
 import dev.toma.configuration.config.ConfigHolder;
 import dev.toma.configuration.config.format.ConfigFormats;
 import dev.toma.configuration.config.format.IConfigFormatHandler;
 import dev.toma.configuration.config.io.ConfigIO;
-import dev.toma.configuration.config.value.ConfigValue;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.gui.screens.Screen;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
-import java.util.Map;
 
 public final class Configuration implements ModInitializer {
 
@@ -30,7 +20,7 @@ public final class Configuration implements ModInitializer {
 
     public Configuration() {
         if (isDevelopmentEnvironment()) {
-            registerConfig(TestingConfig.class, ConfigFormats.yaml());
+            registerConfig(TestingConfig.class, ConfigFormats.properties());
         }
     }
 
@@ -38,17 +28,6 @@ public final class Configuration implements ModInitializer {
     public void onInitialize() {
         ConfigIO.FILE_WATCH_MANAGER.startService();
     }
-
-    /**
-     * Registers your config class. Config will be immediately loaded upon calling.
-     *
-     * @param cfgClass Your config class
-     * @param formatFactory File format to be used by this config class. You can use values
-     *                      from {@link ConfigFormats} for example.
-     * @return Config holder containing your config instance. You obtain it by calling
-     * {@link ConfigHolder#getConfigInstance()} method.
-     * @param <CFG> Config type
-     */
     public static <CFG> ConfigHolder<CFG> registerConfig(Class<CFG> cfgClass, IConfigFormatHandler formatFactory) {
         Config cfg = cfgClass.getAnnotation(Config.class);
         if (cfg == null) {
@@ -71,65 +50,6 @@ public final class Configuration implements ModInitializer {
         return holder;
     }
 
-    /**
-     * You can obtain default config screen based on provided config class.
-     *
-     * @param configClass Your config class
-     * @param previous Previously open screen
-     * @return Either new config screen or {@code null} when no config exists for the provided class
-     */
-    @Nullable
-    @Environment(EnvType.CLIENT)
-    public static Screen getConfigScreen(Class<?> configClass, Screen previous) {
-        Config cfg = configClass.getAnnotation(Config.class);
-        if (cfg == null) {
-            return null;
-        }
-        String id = cfg.id();
-        return getConfigScreen(id, previous);
-    }
-
-    /**
-     * You can obtain default config screen based on provided config ID.
-     *
-     * @param configId ID of your config
-     * @param previous Previously open screen
-     * @return Either new config screen or {@code null} when no config exists with the provided ID
-     */
-    @Nullable
-    @Environment(EnvType.CLIENT)
-    public static Screen getConfigScreen(String configId, Screen previous) {
-        return ConfigHolder.getConfig(configId)
-                .map(holder -> getConfigScreenForHolder(holder, previous))
-                .orElse(null);
-    }
-
-    /**
-     * Obtain group of multiple configs based on group ID. This is useful when you have multiple config files
-     * for your mod.
-     *
-     * @param group Group ID, usually mod ID
-     * @param previous Previously open screen
-     * @return Either new config group screen or null when no config exists under the provided group
-     */
-    @Environment(EnvType.CLIENT)
-    public static Screen getConfigScreenByGroup(String group, Screen previous) {
-        List<ConfigHolder<?>> list = ConfigHolder.getConfigsByGroup(group);
-        if (list.isEmpty())
-            return null;
-        return getConfigScreenByGroup(list, group, previous);
-    }
-
-    @Environment(EnvType.CLIENT)
-    public static Screen getConfigScreenForHolder(ConfigHolder<?> holder, Screen previous) {
-        Map<String, ConfigValue<?>> valueMap = holder.getValueMap();
-        return new ConfigScreen(holder.getConfigId(), holder.getConfigId(), valueMap, previous);
-    }
-
-    @Environment(EnvType.CLIENT)
-    public static Screen getConfigScreenByGroup(List<ConfigHolder<?>> group, String groupId, Screen previous) {
-        return new ConfigGroupScreen(previous, groupId, group);
-    }
 
     private static boolean isDevelopmentEnvironment() {
         return FabricLoader.getInstance().isDevelopmentEnvironment();
